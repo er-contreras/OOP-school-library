cb86bffc2fdcc077c43b8ba2c495a48bbc530a01
Erick Contreras <eri89--@hotmail.com>
2021-10-25 18:37:23 -0500

    Add classes person, student and teacher

---
+++ person.rb
@@ -0,0 +1,27 @@
+require './student'
+
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    if @age >= 21
+      true
+    elsif @parent_permission == true
+      false
+    end
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
