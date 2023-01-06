7346c9f72778db7f7c5414ddea9a5a3c9447eaec
Erick Contreras <eri89--@hotmail.com>
2023-01-05 19:23:14 -0600

    Add Classroom class has many students

--- main.rb
+++ main.rb
@@ -3,12 +3,15 @@
 require_relative 'person'
 require_relative 'student'
 require_relative 'teacher'
+require_relative 'classroom'
 
 student = Student.new('erick', 'B2', 12)
+student2 = Student.new('Francis', 'B2', 14)
 teacher = Teacher.new('Mateo Sin Apellido', 42, 'Doctor')
-p student.validate_name
-p teacher.validate_name
 
-p student.name
-p teacher.name
+classroom = Classroom.new('B4')
+classroom.add_student(student)
+classroom.students[0].classroom.label
+
+p student.classroom.students[0].name
 
